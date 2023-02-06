5d71d33da64da06ca59e376fb2149e6868a30236
Juan Sebastian Espinola <sebar5er@gmail.com>
2023-02-06 16:23:32 -0300

    add .graphql type defs file

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,22 +1,24 @@
 const { ApolloServer } = require('@apollo/server')
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('@apollo/server-plugin-landing-page-graphql-playground')
 const { expressMiddleware  } = require('@apollo/server/express4');
-
-const typeDefs = `
-  type Query {
-    hello: String
-  }
-`
+const { loadFiles }  = require('@graphql-tools/load-files')
 
 const resolvers = {
   Query: {
-    hello: () => 'Hola mundo'
+    getProduct: () => ({
+      id: '1e212',
+      name: 'Azucar',
+      description: 'Dulcecita',
+      price: 113,
+      image: 'https://juanse.dev/logo.png',
+      createdAt: new Date().toISOString()
+    })
   }
 }
 
 const useGraphql = async (app) => {
   const server = new ApolloServer({
-    typeDefs,
+    typeDefs: await loadFiles('./src/**/*.graphql'),
     resolvers,
     playground: true,
     plugins: [
